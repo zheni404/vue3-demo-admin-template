5d1ed4629adb7ecd3c98016038779b828fcc15fa
zheni404 <zheni404@gmail.com>
2021-06-24 11:02:32 +0300

    Init project

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>This is an <b>about</b> page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class About extends Vue {}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>This is an <b>home</b> page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class Home extends Vue {}
+</script>
